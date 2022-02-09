29688762cfb3e05af389fc8f03ec7c3d37a71441
William Ayd <william.ayd@icloud.com>
2022-02-09 15:09:27 -0800

    update setup

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Office/Business",
         "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
